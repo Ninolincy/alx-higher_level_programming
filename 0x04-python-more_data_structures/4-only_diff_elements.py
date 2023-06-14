601d5bf131d167a4e53efada1fad93b5110078ea
Ninolincy <lincyakoth@gmail.com>
2023-06-15 01:02:02 +0300

    Present in only one set

--- 0x04-python-more_data_structures/4-only_diff_elements.py
+++ 0x04-python-more_data_structures/4-only_diff_elements.py
@@ -1,9 +1,4 @@
 #!/usr/bin/python3
 def only_diff_elements(set_1, set_2):
-    set_3 = set_2
-    for x in set_1:
-        if x in set_3:
-            set_3.remove()
-            continue
-        set_3.add(x)
-        return set_3
+    output = set_1 ^ set_2
+    return output
