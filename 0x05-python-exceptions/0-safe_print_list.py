c118960e809f8a71ca63b1230d4d79998e9b1934
Ninolincy <lincyakoth@gmail.com>
2023-06-26 22:21:53 +0300

    Print list elements

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
+    num = 0
     try:
-        num = 0
         for element in my_list:
             print(element, end='')
             num += 1
