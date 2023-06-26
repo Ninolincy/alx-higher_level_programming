44f882f1baa2b8660be89e0c4ad129eb40fb3bc0
Ninolincy <lincyakoth@gmail.com>
2023-06-26 20:39:43 +0300

    Print list elements

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -4,7 +4,7 @@ def safe_print_list(my_list=[], x=0):
         num = 0
         while num < x:
             print("{}".format(my_list[num]), end='')
-            num +=1
+            num += 1
             print()
             return num
 
