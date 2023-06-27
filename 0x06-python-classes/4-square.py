491e03976a59b2816c61b0588d147fa6d9eccfc0
Ninolincy <lincyakoth@gmail.com>
2023-06-28 00:07:48 +0300

    Square

---
+++ 0x06-python-classes/4-square.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+'''Define a square'''
+
+
+class Square:
+    '''Represent a square'''
+    def __init__(self, size=0):
+
+        @property
+        def size(self):
+            return self.__size
+
+        @size.setter
+        def size(self, value):
+            if not isinstance(value, int):
+                raise TypeError("size must be an integer")
+            elif size < 0:
+                raise ValueError("size must be >= 0")
+            self.__size = size
+
+    def area(self):
+        return self.__size ** 2
