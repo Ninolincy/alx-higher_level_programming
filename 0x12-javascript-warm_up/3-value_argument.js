21345cb43a4b13e6cd12f2020498756dff20dbd8
Ninolincy <lincy3akoth@gmail.com>
2024-01-08 15:06:38 +0300

    Js warm-up

--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -1,8 +1,7 @@
 #!/usr/bin/node
 
 if (process.argv[2]) {
-    console.log(process.argv[2]);
-}
-else {
-    console.log('No argument');
+  console.log(process.argv[2]);
+} else {
+  console.log('No argument');
 }
