c7c1ef98f62a604c099b12a1f90dd246213038f5
Ninolincy <lincy3akoth@gmail.com>
2024-01-14 21:35:01 +0300

    Insert value

--- 0x0D-SQL_introduction/7-insert_value.sql
+++ 0x0D-SQL_introduction/7-insert_value.sql
@@ -2,7 +2,5 @@
 --row data,
 --  id = 89
 --  name = Best School
-INSERT INTO 'first_table' (
-    id = "89",
-    name = "Best School"
-);
+INSERT INTO 'first_table' ('id', 'name')
+VALUES ("89", "Best School");
