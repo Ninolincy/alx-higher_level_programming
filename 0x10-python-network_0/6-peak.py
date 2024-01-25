51b3e6d8d38388fad0675eebb8ff5fb4928ad58b
Ninolincy <lincy3akoth@gmail.com>
2024-01-25 16:37:36 +0300

    peak

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
+"""finds a peak in a list of unsorted integers"""
+
 
 def find_peak(list_of_integers):
-    """finds a peak in a list of unsorted integers"""
     if not list_of_integers:
         return None
     return max(list_of_integers)
