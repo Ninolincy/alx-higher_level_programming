8e9cc0b81c4967f43f1471c92f75bcdb8d11c05a
Ninolincy <lincy3akoth@gmail.com>
2024-01-26 03:49:06 +0300

    body

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,3 +1,3 @@
-#/bin/bash
-# Takes URL, sends a GET request to it, display body size of the response
+#!/bin/bash
+# Takes URL, sends a GET request to it, display body size of response
 curl -sL "$1"
