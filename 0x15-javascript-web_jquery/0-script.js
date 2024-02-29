7d522bdc276c5f34bf49599b901379a30792a940
Ninolincy <lincy3akoth@gmail.com>
2024-02-29 08:10:54 +0300

    JS query mandatory tasks

--- 0x15-javascript-web_jquery/0-script.js
+++ 0x15-javascript-web_jquery/0-script.js
@@ -1,4 +1,4 @@
 #!/usr/bin/node
 
-const element = document.querySelector('header');
-element.style.color = '#FF0000';
+const header = document.querySelector('header');
+header.style.color = '#FF0000';
